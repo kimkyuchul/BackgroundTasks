8b948015a3e6e7152f73e701289b7939a57365a5
kyuchul kim <rbcjf122@gmail.com>
2024-07-09 10:39:52 +0900

    init

---
+++ BackgroundTasksTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BackgroundTasksTest
+//
+//  Created by kyuchul on 7/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
